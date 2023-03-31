3cf8a8c13ba748e59c61666cbed76c494219de56
Navid Hashemi <123667533+Navidhashemicodes@users.noreply.github.com>
2023-03-30 19:28:52 -0700

    Add files via upload

---
+++ src/conf_bound/Conf_apply.m
@@ -0,0 +1,21 @@
+function conf_threshold = Conf_apply(Input_Data_pre, Output_Data_pre, model, delta)
+
+len = size(Input_Data_pre,2);
+system_dim = size(model.biases{end},1);
+
+R = zeros(system_dim, len);
+residual = abs(NN(model, Input_Data_pre) - Output_Data_pre);
+
+
+for j=1:system_dim
+    R(j,:) = sort(residual(j,:));
+end
+
+loc = floor((len+1)*delta)+1;
+
+if loc>len
+    error('Not enough data for Conformal Inference')
+end
+conf_threshold = R(:,loc);
+
+end
